5090c7a8866fcc1c4baf2fe8928de1e6fb6ea211
SrinSS01 <srinjoy.s.01.in@gmail.com>
2022-12-31 14:15:55 +0530

    no comment

--- src/main/java/me/srin/boots/Main.java
+++ src/main/java/me/srin/boots/Main.java
@@ -5,15 +5,15 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.security.auth.login.LoginException;
 
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -21,25 +21,25 @@ import java.util.Random;
 import java.util.Scanner;
 
 import static net.dv8tion.jda.api.requests.GatewayIntent.*;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.EMOTE;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.VOICE_STATE;
+import static net.dv8tion.jda.api.utils.cache.CacheFlag.*;
+import static net.dv8tion.jda.api.utils.cache.CacheFlag.SCHEDULED_EVENTS;
 
 public final class Main extends ListenerAdapter {
     private static final Main INSTANCE = new Main();
     private static final Random RANDOM = new Random();
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        String token = "OTcxMzkzMTEwNjkxMTc2NDU4.Gg7ntp.2DVFNxYWeLtKz1lyEaZT4603ovowkv3SNuonLs";
+    public static void main(String[] args) throws InterruptedException {
+        String token = "OTcxMzkzMTEwNjkxMTc2NDU4.GnK6Ga.54MmlJ6HQ4mLJGjsFIsXCHIH3g2MnjfZUECH58";
         var jda = JDABuilder.createDefault(
-                token,
+                token
+        ).setEnabledIntents(
                 GUILD_MESSAGES,
                 GUILD_MESSAGE_REACTIONS,
-                GUILD_VOICE_STATES,
-                GUILD_EMOJIS
+                GUILD_VOICE_STATES
         ).addEventListeners(INSTANCE)
                 .setStatus(OnlineStatus.ONLINE)
-                .setActivity(Activity.of(Activity.ActivityType.DEFAULT, "meow meow"))
-                .disableCache(VOICE_STATE, EMOTE)
+                .setActivity(Activity.playing("meow meow"))
+                .disableCache(VOICE_STATE, EMOJI, STICKER, SCHEDULED_EVENTS)
                 .build();
         for (Guild guild : jda.awaitReady().getGuilds()) {
             guild.upsertCommand("boots", "shows random boots pics")
@@ -64,11 +64,11 @@ public final class Main extends ListenerAdapter {
     }
 
     @Override
-    public void onSlashCommand(@NotNull SlashCommandEvent event) {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         String bootsUrl = "https://cdn.fundy.tv/Boots/Boots%d.jpg";
         if (!Objects.requireNonNull(event.getGuild())
                 .getSelfMember()
-                .hasPermission(event.getTextChannel(), Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_WRITE, Permission.MESSAGE_READ)) {
+                .hasPermission(event.getGuildChannel(), Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_SEND, Permission.MESSAGE_HISTORY)) {
             event.reply("sowwy I don't have permissions to send message here ;w;").setEphemeral(true).queue();
             return;
         }
@@ -84,9 +84,9 @@ public final class Main extends ListenerAdapter {
                 number = Integer.parseInt(numberString);
             }
             event.reply(bootsUrl.formatted(number)).queue(it -> it.retrieveOriginal().queue(message -> {
-                message.addReaction("bootsHi:971428204181069834").queue();
-                message.addReaction("fundyLove:971428204130729984").queue();
-                message.addReaction("fundyFeelsGood:760830070842458123").queue();
+                message.addReaction(Emoji.fromFormatted("<bootsHi:971428204181069834>")).queue();
+                message.addReaction(Emoji.fromFormatted("<fundyLove:971428204130729984>")).queue();
+                message.addReaction(Emoji.fromFormatted("<fundyFeelsGood:760830070842458123>")).queue();
             }));
         }
     }
